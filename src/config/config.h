d5acc2b10f399f8a7e465f63bb23d4861c921520
noelleputra <noellenputra@gmail.com>
2026-07-06 21:50:17 +0700

    Linter

--- src/config/config.h
+++ src/config/config.h
@@ -4,10 +4,8 @@
 
 namespace config {
     constexpr uint8_t NODE_ID = 2;
-    
-    constexpr uint32_t UART_BAUD = 9600; // UART Baud
+    constexpr uint32_t UART_BAUD = 9600;
     constexpr uint8_t UART_BUFFER_SIZE = 32;
-
     constexpr uint8_t NO_DIRECTION_PIN = 0xFF;
-    constexpr uint8_t RS485_TURNAROUND_US = 200; // 1 bit time at 9600 baud, safe for RS485 turnaround
+    constexpr uint8_t RS485_TURNAROUND_US = 200;
 }
