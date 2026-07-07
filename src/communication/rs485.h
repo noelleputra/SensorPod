08c3813e33023bd278178ca3dd3e6ce81c215fe5
noelleputra <noellenputra@gmail.com>
2026-07-07 13:32:42 +0700

    Small Fixes

--- src/communication/rs485.h
+++ src/communication/rs485.h
@@ -3,18 +3,11 @@
 #include <Arduino.h>
 #include "config/config.h"
 
-class Rs485
-{
+class Rs485 {
 public:
-    void begin(
-        long baud,
-        uint8_t dirPin = config::NO_DIRECTION_PIN
-    );
+    void begin(long baud, uint8_t dirPin = config::NO_DIRECTION_PIN);
     bool requestReceived();
-    void sendPacket(
-        uint8_t soil1,
-        uint8_t soil2
-    );
+    void sendPacket(uint8_t soil1, uint8_t soil2);
 
 private:
     void setTransmitMode();
